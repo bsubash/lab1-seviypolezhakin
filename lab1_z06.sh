2c62fa3314f4c41d9ba50fd9dad3e817375e0661
seviypolezhakin <polezhakinseva@gmail.com>
2019-03-05 12:31:58 +0300

    file change mod

--- lab1_z06.sh
+++ lab1_z06.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "Вывести имя скрипта, который был запущен."
-echo "Name of script - $0"
-echo 
+if [ -n "$1" ] && [ "$1" == "-task" ]
+then
+	echo "Задание"
+	echo "6. Вывести имя скрипта, который был запущен."
+	echo
+fi
+	echo "Name of script - $0"
+exit 0
 
