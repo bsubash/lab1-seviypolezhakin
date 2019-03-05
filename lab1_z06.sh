b2227bc9806823fc021369b2158436d247c6b258
seviypolezhakin <polezhakinseva@gmail.com>
2019-03-05 12:51:35 +0300

    script fir the first lab was added

--- lab1_z06.sh
+++ lab1_z06.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-if [ -n "$1" ] && [ "$1" == "-task" ]
-then
-	echo "Задание"
-	echo "6. Вывести имя скрипта, который был запущен."
-	echo
+if [ -n "$1" ] && [ "$1"  == "-task" ]; then
+echo "Задание"
+echo "6. Вывести имя скрипта, который был запущен."
+echo 
 fi
-	echo "Name of script - $0"
+echo
+echo "Name of script - $0"
 exit 0
-
