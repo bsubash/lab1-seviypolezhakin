5e6d17deac74b9e7107bb2f6b2800dfa517d7766
seviypolezhakin <polezhakinseva@gmail.com>
2019-04-02 14:13:16 +0300

    one of the scripts

---
+++ lab2/lab02_z09.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+arr=($@)
+i=0
+while [ $i -lt $# ]
+do
+  if [ "${arr[$i]}" == "-task" ];
+  then
+    echo "Задание"
+    echo "9. Написать скрипт, который скачивает заглавную страницу известного портала и архивирует ее (использовать wget и tar -czvf )."
+    echo ""
+    break
+  fi
+  ((i++))
+done
+
+wget -r -k -l 7 -p -E -nc  -O sitename.tar https://store.steampowered.com/?l=russian
